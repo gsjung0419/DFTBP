71946b452ad151ef4418c14a0ab5c8ed2eea0554
gsjung0419 <gs4phone@gmail.com>
2022-01-24 13:53:02 -0500

    check

--- lib/dftbp/Install.py
+++ lib/dftbp/Install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Install.py tool to download, unpack, build, and link to the LATTE library
+Install.py tool to download, unpack, build, and link to the DFTBP library
 used to automate the steps described in the README file in this dir
 """
 
@@ -19,8 +19,10 @@ parser = ArgumentParser(prog='Install.py',
 
 version = '1.2.1'
 suffix = 'gfortran'
+#gfortran was not tested with DFTBP 
 
-# known checksums for different LATTE versions. used to validate the download.
+
+# known checksums for different DFTBP versions. used to validate the download.
 checksums = { \
         '1.1.0' : '533635721ee222d0ed2925a18fb5b294', \
         '1.2.0' : '68bf0db879da5e068a71281020239ae7', \
@@ -30,32 +32,32 @@ checksums = { \
 # help message
 
 HELP = """
-Syntax from src dir: make lib-latte args="-b"
-                 or: make lib-latte args="-p /usr/local/latte"
-                 or: make lib-latte args="-m gfortran"
-                 or: make lib-latte args="-b -v 1.2.1"
+Syntax from src dir: make lib-dftbp args="-b"
+                 or: make lib-dftbp args="-p /usr/local/dftbp"
+                 or: make lib-dftbp args="-m gfortran"
+                 or: make lib-dftbp args="-b -v 1.2.1"
 
 Syntax from lib dir: python Install.py -b
-                 or: python Install.py -p /usr/local/latte
+                 or: python Install.py -p /usr/local/dftbp
                  or: python Install.py -m gfortran
                  or: python Install.py -v 1.2.1 -b
 
 Example:
 
-make lib-latte args="-b -m gfortran"   # download/build in lib/latte
-make lib-latte args="-p $HOME/latte"   # use existing LATTE installation
+make lib-dftbp args="-b -m gfortran"   # download/build in lib/dftbp
+make lib-dftbp args="-p $HOME/dftbp"   # use existing DFTBP installation
 """
 
 
 pgroup = parser.add_mutually_exclusive_group()
 pgroup.add_argument("-b", "--build", action="store_true",
-                    help="download and build the LATTE library")
+                    help="download and build the DFTBP library")
 pgroup.add_argument("-p", "--path",
-                    help="specify folder of existing LATTE installation")
+                    help="specify folder of existing DFTBP installation")
 parser.add_argument("-m", "--machine", choices=['gfortran', 'ifort', 'linalg', 'serial', 'mpi'],
                     help="suffix of a Makefile.lammps.* file used for linking LAMMPS with this library")
 parser.add_argument("-v", "--version", default=version,
-                    help="set version of LATTE to download and build (default: %s)" % version)
+                    help="set version of DFTBP to download and build (default: %s)" % version)
 
 args = parser.parse_args()
 
@@ -73,60 +75,60 @@ suffixflag = args.machine is not None
 suffix = args.machine
 
 if pathflag:
-  lattedir = args.path
-  if not os.path.isdir(lattedir):
-    sys.exit("LATTE path %s does not exist" % lattedir)
-  lattedir = fullpath(lattedir)
+  dftbpdir = args.path
+  if not os.path.isdir(dftbpdir):
+    sys.exit("DFTBP path %s does not exist" % dftbpdir)
+  dftbpdir = fullpath(dftbpdir)
 
-homedir = "LATTE-%s" % version
-
-if buildflag:
-  url = "https://github.com/lanl/LATTE/archive/v%s.tar.gz" % version
-  lattepath = fullpath(homepath)
-  lattedir = os.path.join(lattepath, homedir)
-
-# download and unpack LATTE tarball
+homedir = "DFTBP-%s" % version
 
 if buildflag:
-  print("Downloading LATTE ...")
-  geturl(url, "LATTE.tar.gz")
+  url = "https://github.com/gsjung0419/DFTBP/archive/v%s.tar.gz" % version
+  dftbppath = fullpath(homepath)
+  dftbpdir = os.path.join(dftbppath, homedir)
+
+# download and unpack DFTBP tarball
+
+if buildflag:
+  print("Downloading DFTBP ...")
+  geturl(url, "DFTBP.tar.gz")
 
   # verify downloaded archive integrity via md5 checksum, if known.
   if version in checksums:
-    if not checkmd5sum(checksums[version], 'LATTE.tar.gz'):
-      sys.exit("Checksum for LATTE library does not match")
+    if not checkmd5sum(checksums[version], 'DFTBP.tar.gz'):
+      sys.exit("Checksum for DFTBP library does not match")
 
-  print("Unpacking LATTE ...")
-  if os.path.exists(lattedir):
-    shutil.rmtree(lattedir)
-  if tarfile.is_tarfile('LATTE.tar.gz'):
-    tgz = tarfile.open('LATTE.tar.gz')
+  print("Unpacking DFTBP ...")
+  if os.path.exists(dftbpdir):
+    shutil.rmtree(dftbpdir)
+  if tarfile.is_tarfile('DFTBP.tar.gz'):
+    tgz = tarfile.open('DFTBP.tar.gz')
     tgz.extractall()
-    os.remove('LATTE.tar.gz')
+    os.remove('DFTBP.tar.gz')
   else:
-    sys.exit("File LATTE.tar.gz is not a supported archive")
+    sys.exit("File DFTBP.tar.gz is not a supported archive")
 
-  # build LATTE
-  print("Building LATTE ...")
-  cmd = 'cd "%s"; make' % lattedir
+  # build DFTBP
+  print("Building DFTBP ...")
+  cmd = 'cd "%s"; make' % dftbpdir
   try:
     txt = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     print(txt.decode('UTF-8'))
   except subprocess.CalledProcessError as e:
     sys.exit("Make failed with:\n %s" % e.output.decode('UTF-8'))
 
-# create 3 links in lib/latte to LATTE dirs
+# create 3 links in lib/dftbp to DFTBP dirs
 
-print("Creating links to LATTE files")
+print("Creating links to DFTBP files")
 if os.path.isfile("includelink") or os.path.islink("includelink"):
   os.remove("includelink")
 if os.path.isfile("liblink") or os.path.islink("liblink"):
   os.remove("liblink")
 if os.path.isfile("filelink.o") or os.path.islink("filelink.o"):
   os.remove("filelink.o")
-os.symlink(os.path.join(lattedir, 'src'), 'includelink')
-os.symlink(lattedir, 'liblink')
-os.symlink(os.path.join(lattedir, 'src', 'latte_c_bind.o'), 'filelink.o')
+os.symlink(os.path.join(dftbpdir, 'src'), 'includelink')
+os.symlink(dftbpdir, 'liblink')
+os.symlink(os.path.join(dftbpdir, 'src', 'dftbp_c_bind.o'), 'filelink.o')
 
 # copy Makefile.lammps.suffix to Makefile.lammps
 
